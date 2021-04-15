9e5c5c270bc43a651028aee62a96ac44016577fd
yl9517 <80736033+yl9517@users.noreply.github.com>
2021-04-15 12:28:24 +0900

    Update Review.java

--- web/WebContent/dto/Review.java
+++ web/WebContent/dto/Review.java
@@ -2,57 +2,86 @@ package dto;
 
 public class Review {
 	
-	private String reviewNum;
+	private int reviewNum;
+	private int reviewExNum;
+	private String reviewUserID;
+	private String reviewContent;
+	private String reviewFile;
+	private double reviewGrade;
+	private String reviewDate;
 	
-	public String getReviewNum() {
+	private Review() {
+		
+	}
+	
+	private Review(int reviewNum,int reviewExNum, String reviewUserID, String reviewContent, double reviewGrade,String reviewDate){
+		
+		this.reviewNum = reviewNum;
+		this.reviewExNum=reviewExNum;
+		this.reviewUserID=reviewUserID;
+		this.reviewContent=reviewContent;
+		this.reviewGrade=reviewGrade;
+		this.reviewDate=reviewDate;
+		
+	}
+
+	public int getReviewNum() {
 		return reviewNum;
 	}
-	public void setReviewNum(String reviewNum) {
+
+	public void setReviewNum(int reviewNum) {
 		this.reviewNum = reviewNum;
 	}
-	public String getReviewEXNum() {
-		return reviewEXNum;
+
+	public int getReviewExNum() {
+		return reviewExNum;
 	}
-	public void setReviewEXNum(String reviewEXNum) {
-		this.reviewEXNum = reviewEXNum;
+
+	public void setReviewExNum(int reviewExNum) {
+		this.reviewExNum = reviewExNum;
 	}
+
 	public String getReviewUserID() {
 		return reviewUserID;
 	}
+
 	public void setReviewUserID(String reviewUserID) {
 		this.reviewUserID = reviewUserID;
 	}
+
 	public String getReviewContent() {
 		return reviewContent;
 	}
+
 	public void setReviewContent(String reviewContent) {
 		this.reviewContent = reviewContent;
 	}
+
 	public String getReviewFile() {
 		return reviewFile;
 	}
+
 	public void setReviewFile(String reviewFile) {
 		this.reviewFile = reviewFile;
 	}
-	public String getReviewGrade() {
+
+	public double getReviewGrade() {
 		return reviewGrade;
 	}
-	public void setReviewGrade(String reviewGrade) {
+
+	public void setReviewGrade(double reviewGrade) {
 		this.reviewGrade = reviewGrade;
 	}
+
 	public String getReviewDate() {
 		return reviewDate;
 	}
+
 	public void setReviewDate(String reviewDate) {
 		this.reviewDate = reviewDate;
 	}
 	
-	private String reviewEXNum;
-	private String reviewUserID;
-	private String reviewContent;
-	private String reviewFile;
-	private String reviewGrade;
-	private String reviewDate;
+	
 	
 	
 
