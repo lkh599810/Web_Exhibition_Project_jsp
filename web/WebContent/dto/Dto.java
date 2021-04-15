c0e1c2a16df761ca466621ed622d8be837ac95c6
jehyun5876 <80736099+jehyun5876@users.noreply.github.com>
2021-04-15 12:10:41 +0900

    Add files via upload

---
+++ web/WebContent/dto/Dto.java
@@ -0,0 +1,59 @@
+package project;
+
+public class Dto {
+	
+	private String reviewNum;
+	
+	public String getReviewNum() {
+		return reviewNum;
+	}
+	public void setReviewNum(String reviewNum) {
+		this.reviewNum = reviewNum;
+	}
+	public String getReviewEXNum() {
+		return reviewEXNum;
+	}
+	public void setReviewEXNum(String reviewEXNum) {
+		this.reviewEXNum = reviewEXNum;
+	}
+	public String getReviewUserID() {
+		return reviewUserID;
+	}
+	public void setReviewUserID(String reviewUserID) {
+		this.reviewUserID = reviewUserID;
+	}
+	public String getReviewContent() {
+		return reviewContent;
+	}
+	public void setReviewContent(String reviewContent) {
+		this.reviewContent = reviewContent;
+	}
+	public String getReviewFile() {
+		return reviewFile;
+	}
+	public void setReviewFile(String reviewFile) {
+		this.reviewFile = reviewFile;
+	}
+	public String getReviewGrade() {
+		return reviewGrade;
+	}
+	public void setReviewGrade(String reviewGrade) {
+		this.reviewGrade = reviewGrade;
+	}
+	public String getReviewDate() {
+		return reviewDate;
+	}
+	public void setReviewDate(String reviewDate) {
+		this.reviewDate = reviewDate;
+	}
+	
+	private String reviewEXNum;
+	private String reviewUserID;
+	private String reviewContent;
+	private String reviewFile;
+	private String reviewGrade;
+	private String reviewDate;
+	
+	
+
+}
